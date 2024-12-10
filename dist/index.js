5d1013d1f94884229ede504d383f1cf8127246e7
edpau <edpau@me.com>
2024-12-10 13:12:09 +0000

    docs: update README

---
+++ dist/index.js
@@ -0,0 +1,21 @@
+"use strict";
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if (!audio)
+        return;
+    audio.currentTime = 0;
+    audio.play();
+    key === null || key === void 0 ? void 0 : key.classList.add("playing");
+}
+function removeTransition(e) {
+    if (e.propertyName !== "transform")
+        return; // there is more than one TransitionEvent, so we pick the longest one, "transform"
+    console.log(e.propertyName);
+    this.classList.remove("playing");
+}
+const keys = document.querySelectorAll(".key");
+keys.forEach((key) => {
+    key.addEventListener("transitionend", removeTransition);
+});
+window.addEventListener("keydown", playSound);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-const foo = 1
+function playSound(e: KeyboardEvent) {
+  const audio = document.querySelector<HTMLAudioElement>(
+    `audio[data-key="${e.keyCode}"]`
+  );
+  const key = document.querySelector<HTMLDivElement>(
+    `.key[data-key="${e.keyCode}"]`
+  );
+  if (!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+  key?.classList.add("playing");
+}
+
+function removeTransition(this: HTMLDivElement, e: TransitionEvent): void {
+  if (e.propertyName !== "transform") return; // there is more than one TransitionEvent, so we pick the longest one, "transform"
+  this.classList.remove("playing");
+}
+
+const keys = document.querySelectorAll<HTMLDivElement>(".key");
+
+keys.forEach((key) => {
+  key.addEventListener("transitionend", removeTransition);
+});
+
+window.addEventListener("keydown", playSound);
